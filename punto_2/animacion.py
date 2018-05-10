ca00ccd79633a6875c27f4e88315866f49a0a4f5
Sortizv12 <s.ortizv@uniandes.edu.co>
2018-05-10 11:54:25 -0500

    Se realizo la animacion

--- punto_2/animacion.py
+++ punto_2/animacion.py
@@ -17,21 +17,24 @@ fig = plt.figure()
 ax = plt.axes(xlim=(0, 100), ylim=(-1, 1))
 line, = ax.plot([], [], lw=2)
 
+def posiciones(i):
+	return u[i+2]
 # initialization function: plot the background of each frame
 def init():
 	line.set_data([], [])
+	ax.set_xlim(0,100)
+        ax.set_ylim(-1, 1)
 	return line,
 
 # animation function.  This is called sequentially
 def animate(i):
-	y=u
+	y=posiciones(i-2)
 	line.set_data(x, y)
 	return line,
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=20, blit=True)
 plt.show()
-anim.save('cuerda.gif', writer='imagemagick', fps=60)# fps=30)#, extra_args=['-vcodec', 'libx264'])
+anim.save('cuerda.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 
--- punto_2/makefile.mk
+++ punto_2/makefile.mk
@@ -1,3 +1,4 @@
-a.out : cuerda.cpp
+cuerda.gif : cuerda.cpp animacion.py
 	g++ cuerda.cpp
 	./a.out > datos.txt
+	python animacion.py
